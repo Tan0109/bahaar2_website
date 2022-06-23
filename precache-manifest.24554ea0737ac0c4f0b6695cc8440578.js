a8265f4d391140dc000b3e3c9359110d229b3bf6
Tanvee Gandhi <Tanveeg1@gmail.com>
2022-06-23 15:57:26 +0530

    Updates

---
+++ precache-manifest.24554ea0737ac0c4f0b6695cc8440578.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4fbf1ed124b65c0637be666872ca0c",
+    "url": "/bahaar2_website/index.html"
+  },
+  {
+    "revision": "68b158867f225c1c0726",
+    "url": "/bahaar2_website/static/css/main.988d2b61.chunk.css"
+  },
+  {
+    "revision": "36ffef849ebd287091a2",
+    "url": "/bahaar2_website/static/js/2.98509930.chunk.js"
+  },
+  {
+    "revision": "7610a9d8e3924a9f42b39a64f5cc36ee",
+    "url": "/bahaar2_website/static/js/2.98509930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b158867f225c1c0726",
+    "url": "/bahaar2_website/static/js/main.1dfb2838.chunk.js"
+  },
+  {
+    "revision": "a41c2ba44d0037d1d7e8",
+    "url": "/bahaar2_website/static/js/runtime-main.a2f40c19.js"
+  },
+  {
+    "revision": "4e38a4fe0c70480586dbdde1350aae6f",
+    "url": "/bahaar2_website/static/media/ad.4e38a4fe.mp4"
+  },
+  {
+    "revision": "fb08e3fbffea42a1f75cc091e3648893",
+    "url": "/bahaar2_website/static/media/banner.fb08e3fb.jpg"
+  },
+  {
+    "revision": "940669b57d9c7548060f6634c7ac7aa4",
+    "url": "/bahaar2_website/static/media/banner2.940669b5.jpg"
+  },
+  {
+    "revision": "88bdb8ceec25632daf22c5327c301ee9",
+    "url": "/bahaar2_website/static/media/banner3.88bdb8ce.jpg"
+  },
+  {
+    "revision": "83f94f41935a0b29ed7de860e40e6bc9",
+    "url": "/bahaar2_website/static/media/icon3.83f94f41.jpg"
+  },
+  {
+    "revision": "fc74124cfe76f5c2926fd9dab6b73f03",
+    "url": "/bahaar2_website/static/media/iframe2.fc74124c.mp4"
+  },
+  {
+    "revision": "c1d84d2720410037bcca4c5e41291faa",
+    "url": "/bahaar2_website/static/media/logo.c1d84d27.png"
+  },
+  {
+    "revision": "c62f1791c7ee3d8d04e4ce4f5efebf4f",
+    "url": "/bahaar2_website/static/media/mam.c62f1791.jfif"
+  },
+  {
+    "revision": "afb52f2dfaeea1829561c46d11be6872",
+    "url": "/bahaar2_website/static/media/ss1.afb52f2d.png"
+  },
+  {
+    "revision": "75ca2fa768c69f803f53af8c1d39c215",
+    "url": "/bahaar2_website/static/media/ss2.75ca2fa7.png"
+  },
+  {
+    "revision": "6ec05e898d11c1f889bfdbe45f665ae2",
+    "url": "/bahaar2_website/static/media/ss3_replace.6ec05e89.png"
+  },
+  {
+    "revision": "94fb9c02f77900fc4bb2d253d66d3dac",
+    "url": "/bahaar2_website/static/media/taha.94fb9c02.jfif"
+  },
+  {
+    "revision": "be6b33e4fcc95fc0c7a25bb32f6bc674",
+    "url": "/bahaar2_website/static/media/tanvee.be6b33e4.jpg"
+  },
+  {
+    "revision": "e1e0ee9f0647ce8d7ba8a9c15c9641d4",
+    "url": "/bahaar2_website/static/media/vinod.e1e0ee9f.jpg"
+  },
+  {
+    "revision": "28cedaf63ee6a7d532a5d0c3c522ca1d",
+    "url": "/bahaar2_website/static/media/yashSir.28cedaf6.jpg"
+  }
+]);
